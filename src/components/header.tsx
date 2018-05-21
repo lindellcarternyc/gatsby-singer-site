1592a5e828bc4f2e1ccec42b079df70e6392f7eb
Lindell Carter <lindellcarternyc@gmail.com>
2018-05-21 15:43:46 -0400

    header comp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,85 @@
 import * as React from 'react'
 import Link from 'gatsby-link'
 
-const Header = (props: { siteTitle: string }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
+import styledComponents from 'styled-components'
+
+import '../assets/css/hamburger.css'
+
+const Container = styledComponents.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background: white;
+`
+
+const Wrapper = styledComponents.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+`
+
+const Title = styledComponents.h1`
+  margin: 0;
+`
+
+const Button = styledComponents.button`
+  margin-top: 0.5rem;
+`
+const HeaderButton = (props: { isActive: boolean, onClick: () => void }) => {
+  const className = 'hamburger hamburger--stand '
+  return (
+    <Button
+      className={className + `${props.isActive ? 'is-active' : ''}`}
+      onClick={props.onClick}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          {props.siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+      <span className="hamburger-box">
+        <span className="hamburger-inner" />
+      </span>
+    </Button>
+  )
+}
+
+interface HeaderProps {
+  siteTitle: string
+}
+interface HeaderState {
+  buttonIsActive: boolean
+}
+class Header extends React.Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
+    super(props)
+
+    this.state = {
+      buttonIsActive: false
+    }
+  }
+
+  public render() {
+    return (
+      <Container>
+        <Wrapper>
+          <Title>
+            <Link
+              to="/"
+              style={{
+                color: '#4f6d7a',
+                textDecoration: 'none'
+              }}
+            >
+              {this.props.siteTitle}
+            </Link>
+          </Title>
+          <HeaderButton isActive={this.state.buttonIsActive} onClick={this.onClickButton}/>
+        </Wrapper>
+      </Container>
+    )
+  }
+
+  private onClickButton = () => {
+    this.setState({ buttonIsActive: !this.state.buttonIsActive })
+  }
+}
 
 export default Header
