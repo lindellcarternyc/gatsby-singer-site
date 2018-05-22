add011d164d5f6ca6b0008803c6352806ada1cff
Lindell Carter <lindellcarternyc@gmail.com>
2018-05-22 10:48:37 -0400

    refactor performance cards

--- src/components/performance-cards/performance-card.tsx
+++ src/components/performance-cards/performance-card.tsx
@@ -4,92 +4,39 @@ import styledComponents from 'styled-components'
 import styledComponentsTS from 'styled-components-ts'
 
 // Models
-import {
-  PerformanceModel, PerformanceDatesModel
-} from '../../models/performance-model'
+import { PerformanceModel } from '../../models/performance-model'
 
 // Utils
 import { Tablet } from '../../utils/breakpoints'
 
-const typography = require('../../utils/typography')
-const bodyColor = typography.options.bodyColor
-
 // Components
-const Container = styledComponentsTS<{column?: number}>(styledComponents.div)`
-  border: 1px solid ${bodyColor};
-  text-align: center;
-  padding: 1rem;
-
-  @media(min-width: ${Tablet}) {
-    grid-column-end: span 2;
-    grid-column-start: ${props => props.column}
-  }
-`
-
-const Text = styledComponents.p`
-  margin: 0;
-`
-
-const DateText = Text.extend`
-  font-size: 0.75rem;
-  font-weight: bold;
-`
-
-const Title = styledComponents.h3`
-  margin: 0.25rem;
-`
-const SubTitle = styledComponents.h4`
-  margin: 0.5rem;
-`
-
-const Divider = styledComponents.hr`
-  background: ${bodyColor};
-  color: ${bodyColor};
-`
-
-const Button = styledComponents.button`
-  border: 1px solid ${bodyColor};
-  background: transparent;
-  font-size: 0.75rem;
-  text-transform: uppercase;
-  margin-top: 0.5rem;
-`
-
-const Dates = (props: { month: string, days: string[] }) => {
-  return <DateText>{props.month} | {props.days.join(', ')}</DateText>
-}
-const Year = (props: { year: string }) => (
-  <DateText>{props.year}</DateText>
-)
-
-
-const PerformanceDates = (props: PerformanceDatesModel) => {
-  return (
-    <div>
-      <Dates month={props.month} days={props.days} />
-      <Year year={props.year}/>
-    </div>
-  )
-}
+import Card, {
+  CardButton,
+  CardHeader,
+  CardText
+} from '../card'
 
 interface PerformanceCardProps {
   performance: PerformanceModel
   column?: number
 }
-export default (props: PerformanceCardProps) => {
+const PerformanceCard = (props: PerformanceCardProps) => {
   const {
     dates, title, subtitle, location, venue
   } = props.performance
 
   return (
-    <Container column={props.column}>
-      <PerformanceDates { ...dates }/>
-      <Title>{title}</Title>
-      {subtitle !== undefined && <SubTitle>{subtitle}</SubTitle>}
-      <Divider />
-      <Text>{venue}</Text>
-      <Text>{location.city}, {location.region}</Text>
-      <Button>More Info</Button>
-    </Container>
+    <Card column={props.column}>
+      <CardHeader
+        title={title}
+        meta={{ dates }}
+        subtitle={subtitle}
+      />
+      <CardText>{venue}</CardText>
+      <CardText>{location.city}, {location.region}</CardText>
+      <CardButton label="More Info" />
+    </Card>
   )
 }
+
+export default PerformanceCard
--- src/components/performance-cards/performance-cards.tsx
+++ src/components/performance-cards/performance-cards.tsx
@@ -1,39 +1,23 @@
 import * as React from 'react'
-
 import styledComponents from 'styled-components'
 
-import { PerformanceModel } from '../../models/performance-model'
-
+import Grid from '../grid'
 import PerformanceCard from './performance-card'
 
-import { Tablet } from '../../utils/breakpoints'
+import { PerformanceModel } from '../../models/performance-model'
 
 interface PerformanceCardsProps {
   performances: PerformanceModel[]
 }
-
-const Wrapper = styledComponents.div`
-  margin-top: 1rem;
-  display: grid;
-  grid-row-gap: 1rem;
-  grid-column-gap: 1rem;
-  grid-template-columns: 100%;
-
-  @media(min-width: ${Tablet}) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-`
-
-// const cards: JSX.Element[]
 export default (props: PerformanceCardsProps) => {
   return (
-    <Wrapper>
+    <Grid>
       {props.performances.map(( perf, idx ) => {
         const column: undefined | 2 = (idx === props.performances.length - 1) && idx % 2 === 0
           ? 2 : undefined
         return <PerformanceCard key={idx} performance={perf} column={column}/>
       })}
-    </Wrapper>
+    </Grid>
   )
 }
 
