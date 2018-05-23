af1159e8106759112efe7568b033cceebf79095c
Lindell Carter <lindellcarternyc@gmail.com>
2018-05-23 13:11:43 -0400

    new columns in grid

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react'
 import styledComponents from 'styled-components'
+import styledComponentsTS from 'styled-components-ts'
 
 // Utils
 import { Tablet } from '../utils/breakpoints'
+
+const Column = styledComponentsTS<{column?: number}>(styledComponents.div)`
+  @media(min-width: ${Tablet}) {
+    grid-column-end: span 2;
+    grid-column-start: ${props => props.column}
+  }
+`
 
 const Wrapper = styledComponents.div`
   margin-top: 1rem;
@@ -17,5 +25,11 @@ const Wrapper = styledComponents.div`
 `
 
 export default (props: { children: React.ReactNode }) => {
-  return <Wrapper>{props.children}</Wrapper>
+  const count = React.Children.count(props.children)
+  const columns = React.Children.map(props.children, (child, idx) => {
+    const column = (idx === count - 1 && count % 2 !== 0)
+      ? 2 : undefined
+    return <Column key={idx} column={column}>{child}</Column>
+  })
+  return <Wrapper>{columns}</Wrapper>
 }
