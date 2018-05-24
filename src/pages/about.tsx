0a42bcc6c6f36d1996029a9844f8c4c86f3a4ce7
Lindell Carter <lindellcarternyc@gmail.com>
2018-05-24 02:57:23 -0400

    use graphql data in pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,13 +2,24 @@ import * as React from 'react'
 
 import { Section } from '../components'
 
-import Data from '../data'
-const { about } = Data
-
-const AboutPage = (): JSX.Element => {
+interface AboutPageProps {
+  data: {
+    allAboutTextJson: {
+      edges: Array<{
+        node: {
+          line: string
+        }
+      }>
+    }
+  }
+}
+const AboutPage = (props: AboutPageProps): JSX.Element => {
+  const { data } = props
+  const { allAboutTextJson } = data
   return (
     <Section title="About Lindell">
-      {about.text.map(line => {
+      {allAboutTextJson.edges.map(edge => {
+        const line = edge.node.line
         return <p key={line}>{line}</p>
       })}
     </Section>
@@ -16,3 +27,16 @@ const AboutPage = (): JSX.Element => {
 }
 
 export default AboutPage
+
+
+export const query = graphql`
+  query AboutPageQuery {
+    allAboutTextJson {
+      edges {
+        node {
+          line
+        }
+      }
+    }
+  }
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,29 +8,117 @@ import {
 } from '../components'
 
 import Data from '../data'
-const { performances, media, news, about } = Data
+const { performances, media } = Data
+import { NewsModel } from '../models/news-model'
 
+interface NewsJson {
+  edges: Array<{
+    node: {
+      title: string
+      subtitle: string
+      date: {
+        year: string
+        month: string
+        days: Array<{
+          day: string
+        }>
+      }
+      content: string
+    }
+  }>
+}
 
-const IndexPage = () => (
-  <div>
-    <Section accent title="About Lindell">
-      {about.text.map(line => {
-        return <p key={line}>{line}</p>
-      })}
-    </Section>
-    <Section title="Performances">
-      <PreviewButton label="See All Performances" />
-      <PerformanceCards performances={performances} />
-    </Section>
-    <Section title="Watch & Listen" accent>
-      <PreviewButton label="See All Videos and Recordings" />
-      <MediaCards mediaList={media} />
-    </Section>
-    <Section title="News">
-      <PreviewButton label="See All Latest News" />
-      <NewsCards news={news} />
-    </Section>
-  </div>
-)
+interface IndexPageProps {
+  data: {
+    allAboutTextJson: {
+      edges: Array<{
+        node: {
+          line: string
+        }
+      }>
+    },
+    allNewsJson: NewsJson
+  }
+}
+
+const parseNews = (data: NewsJson): NewsModel[] => {
+  const { edges } = data
+
+  return edges.map(({ node }) => {
+    const { title, subtitle, date, content } = node
+    const { year, month, days } = date
+    return {
+      title,
+      subtitle,
+      content,
+      date: {
+        year,
+        month,
+        days: days.map(d => d.day)
+      }
+    }
+  })
+}
+
+const IndexPage = (props: IndexPageProps) => {
+  const { data } = props
+
+  const {
+    allAboutTextJson,
+    allNewsJson
+  } = data
+  const news = parseNews(allNewsJson)
+
+  return (
+    <div>
+      <Section accent title="About Lindell">
+        {allAboutTextJson.edges.map(edge => {
+          const line = edge.node.line
+          return <p key={line}>{line}</p>
+        })}
+      </Section>
+      <Section title="Performances">
+        <PreviewButton label="See All Performances" />
+        <PerformanceCards performances={performances} />
+      </Section>
+      <Section title="Watch & Listen" accent>
+        <PreviewButton label="See All Videos and Recordings" />
+        <MediaCards mediaList={media} />
+      </Section>
+      <Section title="News">
+        <PreviewButton label="See All Latest News" />
+        <NewsCards news={news} />
+      </Section>
+    </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexPageQuery {
+    allAboutTextJson {
+      edges {
+        node {
+          line
+        }
+      }
+    }
+    allNewsJson {
+      edges {
+        node {
+          title
+          subtitle
+          date {
+            year
+            month
+            days {
+              day
+            }
+          }
+          content
+        }
+      }
+    }
+  }
+`
