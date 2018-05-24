c7ea013fc474aaffabeae5bf7c62f25e155e4812
Lindell Carter <lindellcarternyc@gmail.com>
2018-05-24 15:12:31 -0400

    cleanup

--- src/pages/about/resume.tsx
+++ src/pages/about/resume.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
-import styled from 'styled-components'
-import styledType from 'styled-components-ts'
+
 import { Section } from '../../components/section'
 
 import ResumeTable from '../../components/about/resume'
