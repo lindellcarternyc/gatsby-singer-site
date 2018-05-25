2e542e97d1d882d3b609ecbf684f3221873817ae
Lindell Carter <lindellcarternyc@gmail.com>
2018-05-24 20:45:48 -0400

    cleanup layouts

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,9 +2,7 @@ import * as React from 'react'
 
 import Helmet from 'react-helmet'
 
-import { Header, Footer } from '../components'
-import AboutNav from '../components/about/nav'
-// import './index.css'
+import { Header, AboutNav, Footer } from '../components/layout'
 
 import { TextColor } from '../utils/colors'
 
